c0c4d59be8d8aa532e5a2d6f33ac7bac0ae13406
wizzy31 <iraktaw@gmail.com>
2021-02-23 19:09:35 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,196 @@
+const help = (prefix) => {
+	return `
+    「 *FIG ZAP BOT* 」
+
+𝗜𝗡𝗙𝗢:
+
+ *O BOT SE ENCONTRA: ON-LINE*
+ ▬▬▬▬▬▬▬【☪】▬▬▬▬▬▬▬
+ *DONO*: Wizzy Iraktã
+ ▬▬▬▬▬▬▬【☪】▬▬▬▬▬▬▬
+ *PREFIX*: 「  ${prefix}  」
+ ▬▬▬▬▬▬▬【☪】▬▬▬▬▬▬▬
+ *WHATSAPP*: wa.me/558681140024
+ ▬▬▬▬▬▬▬【☪】▬▬▬▬▬▬▬
+ *INSTAGRAM*: https://www.instagram.com/_wi22y/
+ ▬▬▬▬▬▬▬【☪】▬▬▬▬▬▬▬
+ *GRUPO OFC*: https://chat.whatsapp.com/DPWGq1tLCvRGuAbLNJ3u4s
+ ▬▬▬▬▬▬▬【☪】▬▬▬▬▬▬▬
+
+*OBS: NÃO USE O COMANDO (KICK) COM MAIS DE UMA PESSOA POR VEZ, VOCÊ SERÁ BLOQUEADO!*
+      
+──▒▒▒▒▒────▄████▄─────
+─▒─▄▒─▄▒──███▄█▀──────
+─▒▒▒▒▒▒▒─▐████──█──█──
+─▒▒▒▒▒▒▒──█████▄──────
+─▒─▒─▒─▒───▀████▀─────
+
+🧐 *SOBRE*
+  │
+  ├─ 🐊 ${prefix}info
+  ├─ 🐊 ${prefix}blocklist
+  ├─ 🐊 ${prefix}chatlist
+  ├─ 🐊 ${prefix}ping
+  ├─ 🐊 ${prefix}dono
+  ├─ 🐊 ${prefix}appbot
+  └─ 🐊 ${prefix}bugreport
+
+🧐 *FAZER*
+  │
+  ├─ 🐊 ${prefix}sticker
+  ├─ 🐊 ${prefix}stickergif
+  ├─ 🐊 ${prefix}toimg
+  ├─ 🐊 ${prefix}tomp3
+  ├─ 🐊 ${prefix}bpink
+  ├─ 🐊 ${prefix}marvellogo
+  ├─ 🐊 ${prefix}snowwrite
+  ├─ 🐊 ${prefix}3dtext
+  ├─ 🐊 ${prefix}ninjalogo
+  ├─ 🐊 ${prefix}water
+  ├─ 🐊 ${prefix}firetext
+  ├─ 🐊 ${prefix}logowolf
+  ├─ 🐊 ${prefix}logowolf2
+  ├─ 🐊 ${prefix}phlogo
+  ├─ 🐊 ${prefix}glitch
+  ├─ 🐊 ${prefix}neonlogo
+  ├─ 🐊 ${prefix}neonlogo2
+  ├─ 🐊 ${prefix}lionlogo
+  ├─ 🐊 ${prefix}jokerlogo
+  ├─ 🐊 ${prefix}shadow
+  ├─ 🐊 ${prefix}burnpaper
+  ├─ 🐊 ${prefix}coffee
+  ├─ 🐊 ${prefix}lovepaper
+  ├─ 🐊 ${prefix}woodblock
+  ├─ 🐊 ${prefix}qowheart
+  ├─ 🐊 ${prefix}mutgrass
+  ├─ 🐊 ${prefix}undergocean
+  ├─ 🐊 ${prefix}woodenboards
+  ├─ 🐊 ${prefix}wolfmetal
+  ├─ 🐊 ${prefix}metalictglow
+  ├─ 🐊 ${prefix}8bit
+  ├─ 🐊 ${prefix}ttp
+  ├─ 🐊 ${prefix}herrypotter
+  ├─ 🐊 ${prefix}pubglogo
+  └─ 🐊 ${prefix}quotemaker
+
+🧐 *MEDIA*
+  │
+  ├─ 🐊 ${prefix}trendtwit
+  ├─ 🐊 ${prefix}randomkpop
+  └─ 🐊 ${prefix}ytsearch
+
+🧐 *EDUCAÇÃO*
+  │
+  ├─ 🐊 ${prefix}wiki
+  ├─ 🐊 ${prefix}wikien
+  ├─ 🐊 ${prefix}nulis
+  ├─ 🐊 ${prefix}quotes
+  ├─ 🐊 ${prefix}quotes2
+  └─ 🐊 ${prefix}artinama
+
+🧐 *MAGIC SHELL*
+  │
+  ├─ 🐊 ${prefix}apakah
+  ├─ 🐊 ${prefix}kapankah
+  ├─ 🐊 ${prefix}rate
+  └─ 🐊 ${prefix}bisakah
+
+🧐 *DOWNLOADER*
+  │
+  ├─ 🐊 ${prefix}images
+  ├─ 🐊 ${prefix}ytmp3
+  ├─ 🐊 ${prefix}ytmp4
+  ├─ 🐊 ${prefix}tiktok
+  └─ 🐊 ${prefix}joox
+
+🧐 *MEME*
+  │
+  ├─ 🐊 ${prefix}meme
+  └─ 🐊 ${prefix}memeindo
+
+🧐 *SOM*
+  │
+  ├─ 🐊 ${prefix}play
+  └─ 🐊 ${prefix}tts
+
+🧐 *MÚSICA*
+  │
+  ├─ 🐊 ${prefix}lirik
+  └─ 🐊 ${prefix}chord
+
+🧐 *ISLAM*
+  │
+  └─ 🐊 ${prefix}quran
+
+🧐 *STALK*
+  │
+  ├─ 🐊 ${prefix}tiktokstalk
+  └─ 🐊 ${prefix}igstalk
+
+🧐 *WIBU*
+  │
+  ├─ 🐊 ${prefix}neonime
+  ├─ 🐊 ${prefix}pokemon
+  ├─ 🐊 ${prefix}loli
+  ├─ 🐊 ${prefix}waifu
+  ├─ 🐊 ${prefix}randomanime
+  ├─ 🐊 ${prefix}husbu
+  ├─ 🐊 ${prefix}husbu2
+  ├─ 🐊 ${prefix}wait
+  └─ 🐊 ${prefix}nekonime
+
+🧐 *DIVERSÃO*
+  │
+  ├─ 🐊 ${prefix}alay
+  ├─ 🐊 ${prefix}gantengcek
+  ├─ 🐊 ${prefix}watak
+  ├─ 🐊 ${prefix}hobby
+  ├─ 🐊 ${prefix}game
+  ├─ 🐊 ${prefix}bucin
+  ├─ 🐊 ${prefix}trust
+  ├─ 🐊 ${prefix}dare
+  └─ 🐊 ${prefix}simi
+
+🧐 *INFORMAÇÃO*
+  │
+  ├─ 🐊 ${prefix}bahasa
+  ├─ 🐊 ${prefix}kodenegara
+  ├─ 🐊 ${prefix}kbbi
+  ├─ 🐊 ${prefix}fakta
+  ├─ 🐊 ${prefix}infocuaca
+  ├─ 🐊 ${prefix}infogempa
+  ├─ 🐊 ${prefix}jadwaltvnow
+  └─ 🐊 ${prefix}covid
+
+🧐 *COMANDOS DO WIZZY*
+  │
+  ├─ 🐊 ${prefix}setprefix
+  ├─ 🐊 ${prefix}block
+  ├─ 🐊 ${prefix}bc
+  ├─ 🐊 ${prefix}bcgc
+  ├─ 🐊 ${prefix}clone
+  └─ 🐊 ${prefix}clearall
+
+🧐 *EXTRAS*
+  │
+  ├─ 🐊 ${prefix}send
+  ├─ 🐊 ${prefix}wame
+  ├─ 🐊 ${prefix}virtex
+  ├─ 🐊 ${prefix}exe
+  ├─ 🐊 ${prefix}qrcode
+  ├─ 🐊 ${prefix}afk
+  ├─ 🐊 ${prefix}timer
+  ├─ 🐊 ${prefix}fml
+  └─ 🐊 ${prefix}fml2
+
+__█████████        
+__█▄█████▄█   
+__█▼▼▼▼▼█ 
+_██ᏴϴͲ Ꮩ3.0██▌  「 *FIM* 」
+__█▲▲▲▲▲█ 
+__█████████ 
+____██_____██
+`
+}
+
+exports.help = help
