972ff7a91ab7e2fec492173e66341b0af2e315d8
wizzy31 <76542649+wizzy31@users.noreply.github.com>
2021-02-24 01:01:02 -0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'FigZap_BOT',
+        ownerName: 'wizzy',
+        youtube: 'https://chat.whatsapp.com/DPWGq1tLCvRGuAbLNJ3u4s',
+        instagram: 'https://www.instagram.com/p/CKjLyuqFKhl/?igshid=1kbanlp9m160j',
+}
