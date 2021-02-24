57b9fe42ef8d67d50a97c1cab27e57dc5f09170b
wizzy31 <76542649+wizzy31@users.noreply.github.com>
2021-02-24 00:26:04 -0300

    Delete config.js

--- settings/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-        botName: 'FIG ZAP BOT',
-        ownerName: 'wizzy',
-        youtube: 'https://youtube.com/channel/UCDfeRz8h2SjZThq8MCPkYqw',
-        instagram: 'https://www.instagram.com/p/CKjLyuqFKhl/?igshid=1rcwcvbua61qg',
-}
